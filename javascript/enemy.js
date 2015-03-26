ef62cfdaef1bac185e7d6958d9397f83ae023857
Todorov96 <todorov.georgi9@abv.bg>
2015-03-26 14:38:05 +0200

    temp

--- javascript/enemy.js
+++ javascript/enemy.js
@@ -1,7 +1,7 @@
 var canvas=document.getElementById("canvas");
 var ctx=canvas.getContext('2d');
 
-var enemy= new Animation(30,19,0,0,4,'sprites/enemy.png',6,1,1);
+var enemy= new Animation(23.3,14,0,0,6,'sprites/enemy.png',1,0,0);
 
 function update(){
     tick();
@@ -9,10 +9,11 @@ function update(){
     requestAnimationFrame(update);
 }
 function tick(){
-     enemy.update();
+    enemy.update();
 }
 function render(ctx){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     enemy.draw(ctx);
 }
-    update();
+update();
+
