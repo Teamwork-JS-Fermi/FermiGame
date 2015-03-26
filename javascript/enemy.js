46909f4831659d23f1f8775d00fdd55724693793
Todorov96 <todorov.georgi9@abv.bg>
2015-03-26 19:28:22 +0200

    EnemyShooting-TO DO

--- javascript/enemy.js
+++ javascript/enemy.js
@@ -88,6 +88,36 @@ for(var i = 1; i <= 40; i++) {
             }
         }
     }
+    function enemyShooting(){
+        //  1  2  3  4  5  6  7  8
+        //  9 10 11 12 13 14 15 16
+        // 17 18 19 20 21 22 23 24
+        // 25 26 27 28 29 30 31 32
+        // 33 34 35 36 37 38 39 40
+        for(var i=33;i<=40;i++){
+            if(!enemyArray[i]){
+                if(!enemyArray[i-8]){
+                    if(!enemyArray[i-16]){
+                        if(!enemyArray[i-24]){
+                            if(enemyArray[i-32]){
+                                //shoot
+                            }
+                        }
+                        else{
+                            //shoot
+                        }
+                    }
+                    else{
+                        //shoot
+                    }
+                }
+                else{
+                    //shoot
+                }
+            }
+        }
+
+    }
     update();
 
 
