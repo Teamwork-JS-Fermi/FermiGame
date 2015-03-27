8c7f31e189c52482f1905e0a9b20c27b3dab2d49
radostpachalieva <radost.pachalieva@gmail.com>
2015-03-27 14:58:21 +0200

    Fixed placeholder. We have to kill some enemys before the score and bonuxs points

---
+++ javascript/Bonus.js
@@ -0,0 +1,41 @@
+//        ============ Score ============
+
+
+
+//        ============ END Score ============
+
+
+
+
+//        ============ High score ============
+//        http://www.html5gamedevs.com/topic/4588-noob-needs-help-creating-a-highscore/
+//        //Globals HERE
+//        var score = 0;
+//        var highscore = 0;
+//
+//        //Update Function HERE
+//
+//        //Psuedo code / Real code
+//
+//        if(player got hit by something (in other terms, he lost the game))
+//        {
+//            //We know before this point of losing the player actually got 'some' points
+//            if (score > localStorage.getItem("highscore")) {
+//                localStorage.setItem("highscore", score);
+//
+//            }
+//
+////If the score is greater than what was stored, then I tell localStorage, "Hey set 'score' as your new stored score as the highscore now
+//
+//
+////After that I display it in the update function when the player clicks "try again" or something of the sort to reset the game
+//
+//            highScoreText.content = 'HIGHSCORE: ' + localStorage.getItem("highscore");
+//        ============ END High score ============
+
+
+
+
+
+
+
