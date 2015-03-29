03227380588fae28b31df35777146ee48178d80a
radostpachalieva <radost.pachalieva@gmail.com>
2015-03-29 22:24:04 +0300

    oh..

--- javascript/enemy.js
+++ javascript/enemy.js
@@ -2,7 +2,7 @@
 //var enemy = new Animation(23.3,14,0,0,6,'sprites/enemy.png',1,0,0);
 var canvas=document.getElementById("canvas");
 var ctx=canvas.getContext('2d');
-var changingHeight = 15; // qwe
+var changingHeight = 14; // qwe
 var changingWidth = 0;
 
 var goingDownArray = {};
@@ -13,14 +13,15 @@ for(var i = 1; i <= 40; i++) {
     goingDownArray[i] = 0;
     posArray[i] = {left: false, right: true, bottom: false};
     if(i<=16){
-        enemyArray[i] = new Animation(23.3, 14, 0, 0, 2, 'sprites/invadersNowfirst.png', 1, 1, 0);
+        enemyArray[i] = new drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh)
+        //enemyArray[i] = new Animation(21, 14, 0, 0, 2, 'sprites/enemy1.png', 1, 0, 1);
     }
     else{
         if(i<=32&&i>16){
-            enemyArray[i] = new Animation(23.3, 14, 0, 0, 2, 'sprites/invadersNowsecond.png', 1, 1, 0);
+            enemyArray[i] = new Animation(21.6, 14, 0, 0, 2, 'sprites/enemy2.png', 1, 0, 1);
         }
         else if(i>32){
-            enemyArray[i] = new Animation(23.3, 14, 0, 0, 2, 'sprites/invadersNowthird.png', 1, 1, 0);
+            enemyArray[i] = new Animation(21.6, 14, 0, 0, 2, 'sprites/enemy3.png', 1, 0, 1);
 
         }
     }
@@ -30,93 +31,89 @@ for(var i = 1; i <= 40; i++) {
     changingWidth += 23.3;
     if(i == 8 || i == 16 || i == 24 || i == 32) {
         changingWidth = 0;
-        changingHeight += 12; // tova e neshtoto koeto sum promenil;    
+        changingHeight += 14; // tova e neshtoto koeto sum promenil;
     }
 }
 
-    function update(){
-        enemyGo();
-        checkPosition();
-        tick();
-        render(ctx);
-        requestAnimationFrame(update);
+function update(){
+    enemyGo();
+    checkPosition();
+    tick();
+    render(ctx);
+    requestAnimationFrame(update);
+}
+function tick(){
+    for(var i = 1; i <= 40; i++) {
+        enemyArray[i].update();
     }
-    function tick(){
-        for(var i = 1; i <= 40; i++) {
-            enemyArray[i].update();
+}
+function render(ctx){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    for(var i = 1; i <= 40; i++) {
+        enemyArray[i].draw(ctx);
+    }
+}
+function checkPosition(){
+    for(var i = 1; i <= 40; i++) {
+        if (enemyArray[i].position.x >= canvas.width - 23.3) {
+            for(var v = 1; v <= 40; v++) {
+                goingDownArray[v] += 2;
+                posArray[v].right = false;
+                posArray[v].left = true;
+                posArray[v].bottom = true;
+            }
+            enemyGo();
         }
-    }
-    function render(ctx){
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        for(var i = 1; i <= 40; i++) {
-            enemyArray[i].draw(ctx);
+        if (enemyArray[i].position.x <= 1.5 && posArray[i].left == true) {
+            for(var j = 1; j <= 40; j++) {
+                goingDownArray[j] += 2;
+                posArray[j].left = false;
+                posArray[j].right = true;
+                posArray[j].bottom = true;
+            }
+            enemyGo();
         }
-    }
-    function checkPosition(){
-        for(var i = 1; i <= 40; i++) {
-            if (enemyArray[i].position.x >= canvas.width - 23.3) {
-                for(var v = 1; v <= 40; v++) {
-                    goingDownArray[v] += 2;
-                    posArray[v].right = false;
-                    posArray[v].left = true;
-                    posArray[v].bottom = true;
-                }
-                enemyGo();
-            }
-            if (enemyArray[i].position.x <= 1.5 && posArray[i].left == true) {
-                for(var j = 1; j <= 40; j++) {
-                    goingDownArray[j] += 2;
-                    posArray[j].left = false;
-                    posArray[j].right = true;
-                    posArray[j].bottom = true;
-                }
-                enemyGo();
-            }
-            if (goingDownArray[i] % 2 == 0 && goingDownArray[i] != 0) {
-                for(var w = 1; w <= 40; w++) {
-                    goingDownArray[w] = 0;
-                    enemyArray[w].position.y + 2;
-                }
-            }
-            if(enemyArray[i].position.y>=canvas.height){
-                //score-- or Game Over
-                //TO DO
+        if (goingDownArray[i] % 2 == 0 && goingDownArray[i] != 0) {
+            for(var w = 1; w <= 40; w++) {
+                goingDownArray[w] = 0;
+                enemyArray[w].position.y + 2;
             }
         }
-    }
-    function enemyGo(){
-        for(var i = 1; i <= 40; i++) {
-            if (posArray[i].left == true) {
-                enemyArray[i].position.x -= 0.5;
-                //enemyArray[i].position.set(enemyArray[i].position.x - 0.5, enemyArray[i].position.y + 0);
-            }
-            if (posArray[i].right == true) {
-                enemyArray[i].position.x += 0.5;
-                //enemyArray[i].position.set(enemyArray[i].position.x + 0.5, enemyArray[i].position.y + 0);
-            }
-            if (posArray[i].bottom == true) {
-                enemyArray[i].position.y += 5;
-                //enemyArray[i].position.set(enemyArray[i].position.x, enemyArray[i].position.y + 5);
-                posArray[i].bottom = false;
-            }
+        if(enemyArray[i].position.y>=canvas.height){
+            //score-- or Game Over
+            //TO DO
         }
     }
-    function enemyShooting(){
-        //  1  2  3  4  5  6  7  8
-        //  9 10 11 12 13 14 15 16
-        // 17 18 19 20 21 22 23 24
-        // 25 26 27 28 29 30 31 32
-        // 33 34 35 36 37 38 39 40
-        for(var i=33;i<=40;i++){
-            if(!enemyArray[i]){
-                if(!enemyArray[i-8]){
-                    if(!enemyArray[i-16]){
-                        if(!enemyArray[i-24]){
-                            if(enemyArray[i-32]){
-                                //shoot
-                            }
-                        }
-                        else{
+}
+function enemyGo(){
+    for(var i = 1; i <= 40; i++) {
+        if (posArray[i].left == true) {
+            enemyArray[i].position.x -= 0.5;
+            //enemyArray[i].position.set(enemyArray[i].position.x - 0.5, enemyArray[i].position.y + 0);
+        }
+        if (posArray[i].right == true) {
+            enemyArray[i].position.x += 0.5;
+            //enemyArray[i].position.set(enemyArray[i].position.x + 0.5, enemyArray[i].position.y + 0);
+        }
+        if (posArray[i].bottom == true) {
+            enemyArray[i].position.y += 5;
+            //enemyArray[i].position.set(enemyArray[i].position.x, enemyArray[i].position.y + 5);
+            posArray[i].bottom = false;
+        }
+    }
+}
+function enemyShooting(){
+    //  1  2  3  4  5  6  7  8
+    //  9 10 11 12 13 14 15 16
+    // 17 18 19 20 21 22 23 24
+    // 25 26 27 28 29 30 31 32
+    // 33 34 35 36 37 38 39 40
+    for(var i=33;i<=40;i++){
+        if(!enemyArray[i]){
+            if(!enemyArray[i-8]){
+                if(!enemyArray[i-16]){
+                    if(!enemyArray[i-24]){
+                        if(enemyArray[i-32]){
                             //shoot
                         }
                     }
@@ -128,10 +125,14 @@ for(var i = 1; i <= 40; i++) {
                     //shoot
                 }
             }
+            else{
+                //shoot
+            }
         }
-
     }
-    update();
+
+}
+update();
 
 
 
